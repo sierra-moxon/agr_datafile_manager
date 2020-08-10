179a44b5dd0f5e4084feb3dd32543cb42cffe360
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2020-08-10 13:12:09 -0700

    add setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import setuptools
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+#!/usr/bin/env python3
 
-setuptools.setup(
-    name="agr-datafile-manager-sierramoxon", # Replace with your own username
-    version="0.0.1",
-    author="Alliance",
-    author_email="help@alliancegenome.org",
-    description="A package used to download and process files from the FMS as well as store neo4j connection information",
-    long_description="A package used to download and process files from the FMS as well as store neo4j connection information",
-    long_description_content_type="text/markdown",
-    url="https://github.com/sierramoxon/agr_datafile_manager",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+from setuptools import setup
+
+setup(name='agr-datafile-manager',
+      version='0.0.1',
+      description='Alliance datafile manager package',
+      url='',
+      author='Alliance',
+      author_email='help@alliancegenome.org',
+      packages=['agr-datafile-manager'],
+      install_requires=[
+          'namedlist',
+          'inflect',
+          'PyYAML',
+          'numpy',
+          'urllib3',
+          'ontobio
+	]
+      zip_safe=False)
